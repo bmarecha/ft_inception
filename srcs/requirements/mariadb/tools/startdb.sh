10cd713cc7573366f96bc223a2a0b6cc7f0bfd99
bmarecha <baptistemarechal42@gmail.com>
2022-04-07 22:12:03 +0200

    before wp redo

--- srcs/requirements/mariadb/tools/startdb.sh
+++ srcs/requirements/mariadb/tools/startdb.sh
@@ -1,9 +1,14 @@
 # Mariadb CONFIG
 service mysql start
-wp core download --allow-root
-echo "CREATE DATABASE wordpress;" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'bmarecha.42.fr' IDENTIFIED BY 'root';" | mysql -u root
+
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'$DOMAIN_NAME' IDENTIFIED BY '$ROOT_PASS';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-mysql wordpress -u root --password=root< /root/wordpress.sql
-echo "GRANT ALL ON *.* TO 'bapt'@'localhost' IDENTIFIED BY '123'" | mysql -u root -password=root
-echo "FLUSH PRIVILEGES;" | mysql -u root -password=root
+echo "GRANT ALL ON *.* TO '$DB_USER'@'$DOMAIN_NAME' IDENTIFIED BY '$DB_UPASS'" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+mysql wordpress -u root < /root/wordpress.sql
+
+service mysql stop
+
+/usr/bin/mysqld_safe
--- srcs/requirements/wordpress/tools/startwp.sh
+++ srcs/requirements/wordpress/tools/startwp.sh
@@ -1,27 +1,25 @@
 # Wordpress CONFIG
 cd
-mkdir /var/www/
-mkdir /var/www/localhost/
+mkdir -p /var/www/localhost/
 cp /root/wordpress.tar.gz /var/www/localhost/
 cd /var/www/localhost/
 tar -xf wordpress.tar.gz
 rm wordpress.tar.gz
 cd
 mv /root/wp-config.php /var/www/localhost/wordpress/
-
+mkdir -p /run/php/
 
 
 # php CONFIG
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-english.tar.gz
-tar -C /var/www/localhost/ -xzf phpMyAdmin-5.0.4-english.tar.gz
+tar -C /var/www/localhost/ -xvzf /root/phpMyAdmin-5.0.4-english.tar.gz --overwrite
 cd
 mv /var/www/localhost/phpMyAdmin-5.0.4-english /var/www/localhost/phpmyadmin
 mv /root/config.inc.php /var/www/localhost/phpmyadmin/
-chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
 chown -R www-data:www-data /var/www/localhost/phpmyadmin
+chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
 
 sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php/7.3/fpm/pool.d/www.conf
 echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 
 
-/usr/sbin/$(ls /usr/sbin | grep php | grep fpm) --nodaemonize
+#/usr/sbin/$(ls /usr/sbin | grep php | grep fpm) --nodaemonize
